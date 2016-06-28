e2bcae6d7558d08a7274f94ab5d0078152b64067
Peter Richardson <m33tar@gmail.com>
2016-06-28 13:55:48 -0400

    more sources

--- unit-tests/sources/external_script.js
+++ unit-tests/sources/external_script.js
@@ -1,8 +1,2 @@
 console.log('external script test');
-console.log('?');
-
-var canvas = document.getElementsByTagName("canvas")[0];
-console.log(canvas);
-var ctx = canvas.getContext("2d");
-ctx.font = "30px Arial";
-ctx.fillText("Hello World",10,50);
+var testvar = "SCRIPT TEST"
